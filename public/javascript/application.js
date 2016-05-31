28885893b769cf809c4f61232204fe6f7ea6c8a9
Garrett <garrettgsb@hotmail.com>
2016-05-31 12:17:23 -0700

    HTML 'dynamically' generated by hard-coded JavaScript; Scripts/CSS loaded

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
-
+  $("h1").html("BOOM DYNAMIC!");
+  createBox();
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
+      //Version 2: Using dynamic variables (no assignment)
+// function createBox() {
+//   $("h1").after(
+//     "<div>" + articleTitle + "</div>"
+//
+//   );}
+
+
+
+      //Version 1: Totally static.
+
+function createBox() {
+  $("h1").after(`
+    <div>
+      <h1>The Title</h1>
+      <img src="academic-sloth.jpg" width="120px">
+      <p>This is what I thought of that one article</p>
+    </div>`);
+}
